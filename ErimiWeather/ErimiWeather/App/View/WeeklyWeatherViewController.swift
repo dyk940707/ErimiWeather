d17b227975e5c141daca2df601cb04340fa6f3db
dykim <doyune0707@gmail.com>
2023-01-27 10:07:45 +0900

    [docs] 디렉토리 분류 및 초기화

---
+++ ErimiWeather/ErimiWeather/App/View/WeeklyWeatherViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WeeklyWeatherViewController.swift
+//  ErimiWeather
+//
+//  Created by 김도윤 on 2023/01/27.
+//
+
+import UIKit
+
+class WeeklyWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
