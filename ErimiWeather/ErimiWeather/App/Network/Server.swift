cd1d072243bd835ad57aa52092376955021e2f5d
dykim <doyune0707@gmail.com>
2023-01-27 10:40:11 +0900

    [feat] 이미지 및 폰트 추가

---
+++ ErimiWeather/ErimiWeather/App/Network/Server.swift
@@ -0,0 +1,12 @@
+//
+//  Server.swift
+//  ErimiWeather
+//
+//  Created by 김도윤 on 2023/01/27.
+//
+
+import Foundation
+
+let weatherServer = "https://apis.data.go.kr"
+
+let weatherServiceKey = "bsjGYN5zW8tUCTzH76e0BQLRPu7OPVqlPesn1va78+xHAfMVaJkX0sOMWbT8e0guca9KXJ5ZW/3I+qoDdiGNfg=="
---
+++ ErimiWeather/ErimiWeather/App/View/TodayWeatherViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TodayWeatherViewController.swift
+//  ErimiWeather
+//
+//  Created by 김도윤 on 2023/01/27.
+//
+
+import UIKit
+
+class TodayWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
