b7c91ef9caff63aba825e54a86cec571d9dafc2a
dykim <doyune0707@gmail.com>
2023-01-31 11:40:19 +0900

    [feat] 오늘 날씨 API 관련 model 추가

---
+++ ErimiWeather/ErimiWeather/App/Model/TodayForecastListModel.swift
@@ -0,0 +1,13 @@
+//
+//  TodayForecastListModel.swift
+//  ErimiWeather
+//
+//  Created by 김도윤 on 2023/01/30.
+//
+
+import Foundation
+
+struct TodayForecastListModel {
+    var time: String
+    var tmp: String
+}
