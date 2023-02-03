c24cc762c7193f15a36e91e016abd313d5fa4933
dykim <doyune0707@gmail.com>
2023-02-03 17:36:09 +0900

    [feat] 주간예보 View 구현중

--- ErimiWeather/ErimiWeather/App/View/TodayWeatherViewController.swift
+++ ErimiWeather/ErimiWeather/App/View/TodayWeatherViewController.swift
@@ -12,6 +12,8 @@ import RxCocoa
 class TodayWeatherViewController: UIViewController {
 
     @IBOutlet weak var todayForecastHeadImageView: UIImageView!
+    @IBOutlet weak var headStateLabel: UILabel!
+    @IBOutlet weak var headTmpLabel: UILabel!
     @IBOutlet weak var todayForecastTableView: UITableView!
     
     private let viewModel = TodayWeatherViewModel(service: WeatherAPIManager.share)
@@ -19,10 +21,25 @@ class TodayWeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        input()
+        output()
+    }
+    
+    private func input() {
         
+    }
+    
+    private func output() {
+        // 오늘 날씨 리스트
         viewModel.todayForecastListSubject.bind(to: todayForecastTableView.rx.items(cellIdentifier: "Cell", cellType: TodayWeatherTableViewCell.self)) { row, element, cell in
             cell.tmpLabel.text = element.tmp
             cell.timeLabel.text = element.time
+        }
+        .disposed(by: disposeBag)
+        
+        viewModel.todayForecastListSubject.subscribe { data in
+            self.headTmpLabel.text = "\(data.element?.first?.tmp ?? "")°"
+            self.headStateLabel.text = "맑음"
         }
         .disposed(by: disposeBag)
     }
